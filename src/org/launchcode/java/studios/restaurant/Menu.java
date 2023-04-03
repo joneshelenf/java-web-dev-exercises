9a2d0220fea3087eb747ab0b14a938f9471348c7
Helen Jones <helenfrancesjones@gmail.com>
2023-04-03 18:47:31 -0500

    restaurant menu studio

---
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.studios.restaurant;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//TODO declare menuItems (ArrayList<menuItem>) and lastUpdated date
+public class Menu {
+    private ArrayList<MenuItem> menuItem = new ArrayList<MenuItem>();
+    private Date lastUpdated;
+
+}
---
+++ src/org/launchcode/java/studios/restaurant/MenuItem.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.studios.restaurant;
+
+import java.util.Date;
+
+//TODO declare private fields name, description, category, price, dateAdded
+public class MenuItem {
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+    private Date dateAdded;
+
+
+//TODO define constructor to take values for first four fields as parameters
+//initialize dateAdded as current date
+
+}
