e2a1cce727cd122d23e6ffb7a6fb53ec6a4626a4
Helen Jones <helenfrancesjones@gmail.com>
2023-04-03 19:08:39 -0500

    add getters and setters

--- src/org/launchcode/java/studios/restaurant/Menu.java
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -4,9 +4,35 @@ package org.launchcode.java.studios.restaurant;
 import java.util.ArrayList;
 import java.util.Date;
 
-//TODO declare menuItems (ArrayList<menuItem>) and lastUpdated date
+//TODO declare menuItems (ArrayList<menuItem>) and lastUpdated date, isNew boolean
 public class Menu {
-    private ArrayList<MenuItem> menuItem = new ArrayList<MenuItem>();
+    private ArrayList<MenuItem> MenuItem = new ArrayList<MenuItem>();
     private Date lastUpdated;
+    private Boolean isNew;
+    public ArrayList<org.launchcode.java.studios.restaurant.MenuItem> getMenuItem() {
+        return MenuItem;
+    }
+
+    public void setMenuItem(ArrayList<org.launchcode.java.studios.restaurant.MenuItem> menuItem) {
+        MenuItem = menuItem;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+
+
 
 }
--- src/org/launchcode/java/studios/restaurant/MenuItem.java
+++ src/org/launchcode/java/studios/restaurant/MenuItem.java
@@ -2,16 +2,68 @@ package org.launchcode.java.studios.restaurant;
 
 import java.util.Date;
 
-//TODO declare private fields name, description, category, price, dateAdded
+//TODO declare private fields name, description, category, price, dateAdded, isNew
 public class MenuItem {
+
     private String name;
     private String description;
     private String category;
     private double price;
     private Date dateAdded;
+    private Boolean isNew;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
 
 
-//TODO define constructor to take values for first four fields as parameters
+
+
+//TODO add getters and setters for each field
 //initialize dateAdded as current date
 
 }
